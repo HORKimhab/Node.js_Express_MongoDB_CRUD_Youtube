b148db5c922a2acaad1570190a522569e2c209a8
Anoskh <kimhab.zeecode@gmail.com>
2020-12-31 12:10:11 +0700

    Model DB

---
+++ models/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const URI = 'mongodb://localhost:27017/EmployeeDB'; 
+const log = console.log; 
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (!err) {
+        log('Connect MongoDB successfully.'); 
+    }
+    else {
+        log('Erro with connection MongoDB: ' + err); 
+    }
+});

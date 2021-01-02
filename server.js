e4cdb7e4172b5b19f41dbf23127f66ae981674a6
Anoskh <kimhab.zeecode@gmail.com>
2021-01-02 11:23:56 +0700

    fix allowInsecurePrototypeAccess is not a function by add allowInsecurePrototypeAccess & unknown params in get list Employees

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express'),
     employeeController = require('./controllers/employeeController'),
     path = require('path'),
     exphbs = require('express-handlebars'),
+    Handlebars = require('handlebars'), 
+    // fix allowInsecurePrototypeAccess is not a function by add allowInsecurePrototypeAccess ---> { allowInsecurePrototypeAccess } 
+    { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access'),
     bodyparser = require('body-parser'); 
 
 let app = express(),
@@ -17,7 +20,13 @@ app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));  
 
 // hbs is extension file name, mainLayout is file name, views/layouts is folder name 
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' })); 
+app.engine('hbs', exphbs({
+    extname: 'hbs', defaultLayout: 'mainLayout',
+    layoutsDir: __dirname + '/views/layouts/',
+    // Issue in Access has been denied to resolve the property
+    //"---" because it is not an "own property" of its parent.
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+})); 
 
 app.set('view engine', 'hbs'); 
 
